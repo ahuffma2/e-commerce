9c90a58c3a9d9104e114b9e4ad953889591ef1b0
Austin Huffman <austinhuffman0@gmail.com>
2021-07-25 15:26:16 -0400

    category route delete finished

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,10 +43,18 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try{
+    const categoryData = await Category.describe({
+      where: { id: req.params.id}
+    });
+  } catch (err) {
+    res.status(500).json(err):
+  }
 });
 
 module.exports = router;
